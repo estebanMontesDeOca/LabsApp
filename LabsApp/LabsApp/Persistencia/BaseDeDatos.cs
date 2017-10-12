06d213ec251b1677f6b14ff189773a2d0a6a6f4e
steineres1995 <31697986+steineres1995@users.noreply.github.com>
2017-10-12 20:49:59 -0300

    Se crea una base de datos temporal

---
+++ LabsApp/LabsApp/Persistencia/BaseDeDatos.cs
@@ -0,0 +1,17 @@
+﻿using LabsApp.Dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabsApp.Persistencia
+{
+    class BaseDeDatos
+    {
+        List<Reserva> listaReservas = new List<Reserva>();
+        List<Persona> listaPersonas = new List<Persona>();
+        List<Laboratorio> listaLaboratorio = new List<Laboratorio>();
+
+    }
+}
