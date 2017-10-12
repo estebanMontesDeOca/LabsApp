c3795be1259fbaee813915980aedf5cec2782da6
steineres1995 <31697986+steineres1995@users.noreply.github.com>
2017-10-11 22:39:39 -0300

    Se agregan Metodos de acceso y modificacion a la calse Laboratorio

--- LabsApp/LabsApp/Dominio/Laboratorio.cs
+++ LabsApp/LabsApp/Dominio/Laboratorio.cs
@@ -8,5 +8,62 @@ namespace LabsApp.Dominio
 {
     class Laboratorio
     {
+        private string nombre;
+        private int numero;
+        private int cantEquipos;
+        private int cantSillas;
+
+        public string Nombre
+        {
+            get
+            {
+                return nombre;
+            }
+            set
+            {
+                string unNombre = value;
+                nombre = unNombre;
+            }
+        }
+
+        public int Numero
+        {
+            get
+            {
+                return numero;
+            }
+            set
+            {
+                int unNumero = value;
+                numero = unNumero;
+            }
+        }
+
+        public int CantEquipos
+        {
+            get
+            {
+                return cantEquipos;
+            }
+            set
+            {
+                int unaCantEquipos = value;
+                cantEquipos = unaCantEquipos;
+            }
+
+        }
+
+        public int CantSillas
+        {
+            get
+            {
+                return cantSillas;
+            }
+            set
+            {
+                int unaCantSillas = value;
+                cantSillas = unaCantSillas;
+            }
+        }
     }
 }
